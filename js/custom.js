d44b084ffac20b5b6af9c42289f30bce34034aba
Geraud <geraud.pereira@hotmail.com>
2023-12-23 13:38:59 -0300

    Aula 2 - Seletores (JQuery I)

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+// instacia jquery e evita conflito
+jquery(function($){
+
+    let titulos = $('h4') // tag
+    let item = $('.featured-items') //class
+    let destaques = $('#featured') // id
+
+})
