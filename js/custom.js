7eafc419454480e1746ebab35650a914691ba9c0
Geraud <geraud.pereira@hotmail.com>
2023-12-23 14:22:15 -0300

    Aula - 3 Manipulação de arquivos HTML (JQuery I)

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,22 @@
 // instacia jquery e evita conflito
-jquery(function($){
+$(document).ready(function(){
+
+    $('.owl-carousel').owlCarousel();
 
     let titulos = $('h4') // tag
+
     let item = $('.featured-items') //class
+
     let destaques = $('#featured') // id
+
+    console.log(titulos.first());
+
+    $('.featured-item a').addClass('btn btn-dark stretch-link');
+
+    $('.featured-item:first h4').append('<span class="badge bg-secondary">Novo</span>');
+
+
+
+
 
 })
